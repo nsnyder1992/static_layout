6eb4e945b5822f6f2f524301932fdba36fd0b93e
Nick Snyder <nsnyder1992@gmail.com>
2020-11-17 14:23:11 -0500

    added more comments in script.js to better understand

--- source/script.js
+++ source/script.js
@@ -1,11 +1,11 @@
 //Change Navbar Color after specified scroll amt
 $(window).scroll(function () {
-    let y = $(window).scrollTop();
+    let scroll = $(window).scrollTop();
     let colorChage = 45;
 
-    if (y > colorChage) {
+    if (scroll > colorChage) {
         $('.navbar').addClass('lightgreen-bg');
-    } else if (y <= colorChage) {
+    } else if (scroll <= colorChage) {
         $('.navbar').removeClass('lightgreen-bg')
     };
 });
@@ -13,27 +13,31 @@ $(window).scroll(function () {
 
 $(document).ready(function() {
 
+    //Open about dropdown close others
     $("#about").click(function() {
         $("#about-content").toggleClass("show");
         $("#help-content").hasClass('show') ? $("#help-content").toggleClass("show") : false
         $("#community-content").hasClass('show') ? $("#community-content").toggleClass("show") : false
     })
 
+    //Open help dropdown close others
     $("#help").click(function() {
         $("#help-content").toggleClass("show");
         $("#about-content").hasClass('show') ? $("#about-content").toggleClass("show") : false
         $("#community-content").hasClass('show') ? $("#community-content").toggleClass("show") : false
     })
 
+    //Open community dropdown close others
     $("#community").click(function() {
         $("#community-content").toggleClass("show");
         $("#help-content").hasClass('show') ? $("#help-content").toggleClass("show") : false
         $("#about-content").hasClass('show') ? $("#about-content").toggleClass("show") : false
     });
 
+    //Closes all dropdowns when something else is clicked
     $(document).click(function(e) {
         let id = e.target.id;
-        console.log(id)
+        //console.log(id)
         if (!$(e.target).hasClass('dropdown-content') && id !== 'about' && id !== 'help' && id !== 'community'){
             $('.dropdown-content').each(function(){
                 $(this).hasClass('show') ? $(this).toggleClass('show') : false
